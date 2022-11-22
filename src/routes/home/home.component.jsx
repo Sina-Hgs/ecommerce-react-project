cacd2f9f502049b4af43567cc05bb245c6d74027
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2022-11-22 14:12:56 +0330

    First version done, adding redirect for Netlify

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,37 +1,9 @@
-import CategoriesContainer from "../../components/categories-container/categories-container.component";
+import CategoriesContainer from "../../components/directory-container/directory.component";
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
     <div>
-      <CategoriesContainer categories={categories} />
+      <CategoriesContainer />
     </div>
   );
 };
